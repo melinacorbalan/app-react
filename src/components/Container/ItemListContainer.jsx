8bbe9513faa956dbd921d8891c7b58a865c7aa39
Melina Corbalan <melina.corbalan@gmail.com>
2021-11-11 23:56:49 -0300

    update arrow icon on homepage

--- src/components/Container/ItemListContainer.jsx
+++ src/components/Container/ItemListContainer.jsx
@@ -3,10 +3,20 @@ import arrow from '../../media/up-arrow-icon.png'
 
 const ItemListContainer = (props) => {
 
+
+    const ScrollUp = () => {
+        window.scrollTo ({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+        })
+    };
+
+
     return (
         <div className="main">
             <h2 className="main-title">Descubre donde se filmaron tus peliculas favoritas!</h2>
-            <img className="arrow-up" src={arrow} alt="arrow up" />
+            <img className="arrow-up" src={arrow} alt="arrow up" onClick={ScrollUp} />
         </div>
     )
 
